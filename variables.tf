32dcd9480e85885496f804c3b262696bfe0f4953
Jason-CloudG <135985363+Jason-CloudG@users.noreply.github.com>
2026-02-25 11:05:59 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
 
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+  default     = "venkatesh-sandbox-new"
+}
+
+variable "region" {
+  description = "GCP Region"
+  type        = string
+  default     = "asia-south1"
+}
